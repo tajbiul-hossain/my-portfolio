cc231f2a2b1ac5c5c4a69eaff6572da55208d493
tajbiul-hossain <tbh.nishat@gmail.com>
2022-05-23 19:26:52 +0600

    updated resume

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-import "bootstrap/dist/css/bootstrap.min.css";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Header from "./pages/Header/Header";
 import Main from "./pages/Main/Main/Main";
@@ -6,8 +5,9 @@ import About from "./pages/About/About";
 import Footer from "./pages/Footer/Footer";
 import Blog from "./pages/Blog/Blog";
 import Explore from "./pages/ExploreProject/Explore";
-import "./App.css";
 import ScrollToTop from "./pages/ScrollTop/ScrollToTop";
+import "./bootstrap/bootstrap.min.css";
+import "./App.css";
 
 function App() {
   return (
